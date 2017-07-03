33479865e65604332be2d18d4bbb8f3baaa2b295
simongeek <szyplotk@student.pg.gda.pl>
2017-07-03 23:06:51 +0200

    1

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Keras Cifar-10 Classification
+
+1. Introduction to Keras library
+2. Describe Cifar-10 with visualization example(matrix 10x5-10)
+3. Describe create process of neural network in Keras - simple 2-layer network
+4. Short introduction to convolutional nets
+5. Describe main results
+6. Post on blog
+"""
