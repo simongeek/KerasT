9869fa4eb3b632330e3a2a0a84db89c13d8c75a2
simongeek <szyplotk@student.pg.gda.pl>
2017-07-04 22:14:54 +0200

    1

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 """
 Keras Cifar-10 Classification
 
+On blog:
+
 1. Introduction to Keras library
 2. Describe Cifar-10 with visualization example(matrix 10x5-10)
 3. Describe create process of neural network in Keras - simple 2-layer network
 4. Short introduction to convolutional nets
 5. Describe main results
 6. Post on blog
+
+
+
+Requirements for the project:
+1. kod wczytujący zbiór cifar
+2. kod dokonujący wizualizacji zbioru, wyświetlenie kilku losowych obrazków ułożonych w macierz wraz z informacją do jakiej należą kategorii
+3. dwie funkcje budujące 2 siecie neuronowe w keras o różnej architekturze (różna ilość warstw, różne funkcje aktywacji, dropout - tak aby pokazać wpływ tych paramterów na ostateczną dokładnośc klasyfikacji)
+4. projekt powinien zawierać wykresy pokazujący progres uczenia się sieci na podstawie  training/testing loss and accuracies co N-itearcji
+5.wyswietlenie wynków klasyfikacji w postaci confusion matrix
+6. plik readme opisujący rozwiązanie min 600 słów w nim dwa
+7. projekt powinien być logicznie podzielony na pliki
+8. kod powinien być obficie skomentowany
+
+
+
 """
+
+from keras.datasets import cifar10
+from matplotlib import pyplot
+from scipy.misc import toimage
+
+#load data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# create a grid of 3x3 images
+
+for i in range(0,9):
+    pyplot.subplot(330 + 1 + i)
+    pyplot.imshow(toimage(X_train[i]))
+# show the plot
+pyplot.show()
