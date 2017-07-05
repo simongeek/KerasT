f3a7f59fe7badd724140f261337d251eedb02606
simongeek <szyplotk@student.pg.gda.pl>
2017-07-05 18:51:31 +0200

    1

--- main.py
+++ main.py
@@ -17,3 +17,23 @@ Requirements for the project:
 7. projekt powinien być logicznie podzielony na pliki
 8. kod powinien być obficie skomentowany
 """
+
+# LOADING THE CIFAR-10 DATASET
+from keras.datasets import cifar10
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+# Simple CNN model for CIFAR-10
+
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
