354644c2b1f1a12b5a45fb01de0020749c42b29b
simongeek <szyplotk@student.pg.gda.pl>
2017-07-08 15:33:03 +0200

    1

--- main.py
+++ main.py
@@ -35,17 +35,29 @@ from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers.normalization import BatchNormalization
 from keras.utils import np_utils
+from keras_sequential_ascii import sequential_model_to_ascii_printout
 from keras import backend as K
 if K.backend()=='tensorflow':
     K.set_image_dim_ordering("th")
 
 # 1. LOADING THE CIFAR-10 DATASET
 from keras.datasets import cifar10
-(train_features, train_labels), (test_features, test_labels) = cifar10.load_data()
+"""(train_features, train_labels), (test_features, test_labels) = cifar10.load_data()
 num_train, img_channels, img_rows, img_cols = train_features.shape
 num_test, _, _, _ = train_features.shape
 num_classes = len(np.unique(train_labels))
+"""
 
+batch_size = 32
+num_classes = 10
+epochs = 200
+data_augmentation = True
+
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 # 2. Here are the classes in the dataset, as well as 10 random images from each
 
 class_names = ['airplane','automobile','bird','cat','deer',
@@ -54,14 +66,20 @@ class_names = ['airplane','automobile','bird','cat','deer',
 fig = plt.figure(figsize=(8,3))
 for i in range(num_classes):
     ax = fig.add_subplot(2, 5, 1 + i, xticks=[], yticks=[])
-    idx = np.where(train_labels[:]==i)[0]
-    features_idx = train_features[idx,::]
+    idx = np.where(y_train[:]==i)[0]
+    features_idx = x_train[idx,::]
     img_num = np.random.randint(features_idx.shape[0])
     im = np.transpose(features_idx[img_num,::],(1,2,0))
     ax.set_title(class_names[i])
     plt.imshow(im)
 plt.show()
 
+
+# Convert
+y_train = np_utils.to_categorical(y_train, num_classes)
+y_test = np_utils.to_categorical(y_test, num_classes)
+
+"""
 # Data Pre-processing
 
 train_features = train_features.astype('float32')/255
@@ -69,11 +87,54 @@ test_features = test_features.astype('float32')/255
 # convert class labels to binary class labels
 train_labels = np_utils.to_categorical(train_labels, num_classes)
 test_labels = np_utils.to_categorical(test_labels, num_classes)
-
+"""
 # 3. Define Model
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(3, 32, 32)))
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32,(3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+sgd = SGD(lr = 0.0001, decay=1e-6, nesterov=True)
+
+# Train
+
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.summary()
+
+# vizualizing model
+sequential_model_to_ascii_printout(model)
+
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train  /= 255
+x_test /= 255
+
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test,y_test),shuffle=True)
+
+scores = model.evaluate(x_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+"""model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(3, 32, 32)))
 model.add(Dropout(0.2))
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
@@ -99,7 +160,7 @@ model.add(Dense(num_classes, activation='softmax'))
 epochs = 25
 lrate = 0.01
 decay = lrate/epochs
-sgd = SGD(lr = lrate, momentum=0.9, decay=decay)
+sgd = SGD(lr = lrate, momentum=0.9, decay=decay, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 model.summary()
 
@@ -109,4 +170,4 @@ model.fit(train_features, train_labels, validation_data=(test_features, test_lab
 
 scores = model.evaluate(test_features, test_labels, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
-
+"""
